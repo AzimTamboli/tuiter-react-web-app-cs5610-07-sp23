caa8e14bc459762c98b401453b03d1f28132367c
Azim <tamboliazim75@gmail.com>
2023-03-26 22:06:01 -0400

    a7 Redux sp23

--- src/labs/index.js
+++ src/labs/index.js
@@ -1,9 +1,18 @@
 import Assignment6 from "./a6";
+import Nav from "../nav";
+import {Route, Routes} from "react-router";
+import Assignment7 from "./a7";
 
 function Labs() {
     return(
         <div>
-            <Assignment6/>
+            <Nav/>
+            <Routes>
+                <Route index
+                       element={<Assignment6/>}/>
+                <Route path="/a7"
+                       element={<Assignment7/>}/>
+            </Routes>
         </div>
     );
 }
